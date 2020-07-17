e0693749515700c98fd01e7b38bc57c48cfaf07f
jcksegal-ns <66098060+jcksegal-ns@users.noreply.github.com>
2020-07-17 09:43:59 -0700

    Update FormHandler.js

--- FormHandler.js
+++ FormHandler.js
@@ -3,6 +3,10 @@ ga(
     function addValuesToMarketoForm() {
         MktoForms2.whenReady( function(form) {
             console.log('Marketo form #'+form.getFormElem()[0].id+' found.')	
+            if (!gaTrackID || !gaClientID) {
+                console.error('Missing GA Tracker ID or GA Client ID');
+                return;
+            }
             form.vals({
                 'GATRACKID__c':gaTrackID,
                 'GACLIENTID__c':gaClientID
@@ -10,8 +14,9 @@ ga(
             console.log('Values populated')
         })
     }
-    var gaTrackID = ga.getAll()[0].get('trackingId')
-    var gaClientID = ga.getAll()[0].get('clientId')
+    var tracker = ga.getAll()[0]
+    var gaTrackID = tracker.get('trackingId')
+    var gaClientID = tracker.get('clientId')
     function initialize() {
         if(typeof MktoForms2 !== 'undefined') {
             addValuesToMarketoForm()
